9bd87e619e4fd5f375b7c07a393225170c35d94f
Jacopo Grassi <jacopo.grassi@polito.it>
2026-02-20 12:28:14 +0100

    -

---
+++ templates/synchronize.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# This step is in charge of syncing the workflow project with the remote platform
+
+set -xuve
+
+# HEADER
+
+HPCROOTDIR=${1:-%HPCROOTDIR%}
+ROOTDIR=${2:-%ROOTDIR%}
+HPCUSER=${3:-%HPCUSER%}
+HPCHOST=${4:-%HPCHOST%}
+PROJDEST=${5:-%PROJECT.PROJECT_DESTINATION%}
+
+# END_HEADER
+
+EXPID=${6:-%EXPID%}
+
+#####################################################
+# Synchronizes file or directory to remote
+# Globals:
+# Arguments:
+#   Remote user
+#   Target host
+#   Source file or directory
+#   Target directory
+#####################################################
+function rsync_to_remote() {
+    echo "rsyncing the dir to the target platform"
+    USR=$1
+    HOST=$2
+    SOURCE=$3
+    DIR=$4
+
+    rsync -avpL "${SOURCE}" "${USR}"@"${HOST}":"${DIR}"/
+}
+
+# MAIN code
+
+cd "${ROOTDIR}"/proj
+
+# If the tar was already sent, we assume that we can update only the changed files in the project
+# The workflow will send the tarball again if the workflow starts over from LOCAL_SETUP
+# if [ ! -f flag_tarball_sent ]; then
+#     rsync_to_remote "${HPCUSER}" "${HPCHOST}" "${PROJDEST}".tar.gz "${HPCROOTDIR}"
+#     rm "${PROJDEST}".tar.gz
+#     touch flag_tarball_sent
+# else
+rsync_to_remote "${HPCUSER}" "${HPCHOST}" "${PROJDEST}"/lib "${HPCROOTDIR}"
+rsync_to_remote "${HPCUSER}" "${HPCHOST}" "${PROJDEST}"/runscripts "${HPCROOTDIR}"
+rsync_to_remote "${HPCUSER}" "${HPCHOST}" "${PROJDEST}"/conf "${HPCROOTDIR}"
+#fi
+
+# MAKE LOG DIR
+# LOG_DIR="${HPCROOTDIR}"/LOG_${EXPID}
+# ssh "${HPCUSER}"@"${HPCHOST}" "mkdir -p ${LOG_DIR}"

85b067cee5df36b9a8899a86d35eda99385fb779
Jacopo Grassi <jacopo.grassi@polito.it>
2026-02-20 12:47:38 +0100

    Update materialize_dep.sh

--- templates/materialize_dep.sh
+++ templates/materialize_dep.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 set -x
 
 # Autosubmit variables (vengono sostituite nel template)
-PROJDIR="%PROJDIR%"
+PROJDIR="%PROJECT.PROJECT_DESTINATION%"
 SUBMODULE_DIR="$PROJDIR/AIUQ-engine"
 
 # Vai nella root progetto (così mkdir/controlli sono coerenti)

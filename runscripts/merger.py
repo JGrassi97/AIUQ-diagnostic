5fec8cb000d6a409f5d1cb8c774738da67b7c04e
Jacopo Grassi <jacopo.grassi@polito.it>
2026-02-23 15:05:14 +0100

    Update merger.py

--- runscripts/merger.py
+++ runscripts/merger.py
@@ -21,12 +21,23 @@ if __name__ == "__main__":
         # Select only nc
         all_files = [file for file in all_files if file.endswith('.nc')]
 
-        # Open and merge
-        with xr.open_mfdataset([f"{_OUTPUT_PATH}/{file}" for file in all_files], combine='by_coords', engine='netcdf4') as dataset:
-                dataset.to_zarr(f"{_OUTPUT_PATH}/ngcm-diagnostic.zarr", consolidated=True)
+        ds = xr.open_mfdataset(
+                all_files,
+                combine="by_coords",
+                engine="netcdf4",
+                data_vars="all",
+                coords="all",
+                join="outer",
+                compat="override",
+                combine_attrs="override",
+                )
 
-        # Remove individual files
-        for file in all_files:
-                os.remove(f"{_OUTPUT_PATH}/{file}")
+        print("DATA_VARS:", list(ds.data_vars))
+        print("COORDS:", list(ds.coords))
 
-        
+        ds.to_zarr(os.path.join(_OUTPUT_PATH, "ngcm-diagnostic.zarr"), consolidated=True)
+        ds.close()
+
+        # # Remove individual files
+        # for file in all_files:
+        #         os.remove(f"{_OUTPUT_PATH}/{file}")

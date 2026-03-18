1fc7bfd8adc525f29dcad767fe970bf42a63634c
Jacopo Grassi <jacopo.grassi@polito.it>
2026-03-18 11:23:48 +0100

    Update probabilistic.py

--- runscripts/probabilistic.py
+++ runscripts/probabilistic.py
@@ -125,9 +125,9 @@ def main() -> None:
         ens_std = model.std(dim="member", ddof=0).rename(f"{var}_std")
         ens_std_real = truth.std(dim="member", ddof=0).rename(f"{var}_std_truth") if "member" in truth.dims else None
 
-        model_centred = model - model.mean(dim="member")
-        model_unitary_std = model / model.std(dim="member", ddof=0)
-        model_normalized = model_centred / model_centred.std(dim="member", ddof=0)
+        model_centred = (model - model.mean(dim="member")) + truth.mean(dim="member")
+        model_unitary_std = (model / model.std(dim="member", ddof=0)) * truth.std(dim="member", ddof=0)
+        model_normalized = model_centred / truth.std(dim="member", ddof=0)
 
         #ens_var = model.var(dim="member", ddof=0).rename(f"{var}_var")
         n = xr.ones_like(ens_std).rename(f"{var}_n")
